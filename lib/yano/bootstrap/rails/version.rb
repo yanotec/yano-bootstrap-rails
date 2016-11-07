05a4e594816cc320966bcfb54c42c28aa6d05fdc
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-11-07 15:04:32 -0200

    Update Fontawesome version

--- lib/yano/bootstrap/rails/version.rb
+++ lib/yano/bootstrap/rails/version.rb
@@ -3,7 +3,7 @@ module Yano
     module Rails
       VERSION = "2.1.1"
       BOOTSTRAP_VERSION = "3.3.7"
-      FONT_AWESOME_VERSION = "4.6.3"
+      FONT_AWESOME_VERSION = "4.7.0"
     end
   end
 end
